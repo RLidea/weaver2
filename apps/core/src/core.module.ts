87d784c693f9760bc46921c12688c628b1f67d7f
Centell <centell@me.com>
2025-06-16 12:27:49 +0900

    feat: add user module

--- apps/core/src/core.module.ts
+++ apps/core/src/core.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { CoreController } from './core.controller';
 import { CoreService } from './core.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
   ],
   controllers: [CoreController],
   providers: [CoreService],
--- libs/common/src/configs/middleware/index.ts
+++ libs/common/src/configs/middleware/index.ts
@@ -1,6 +1,9 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, VersioningType } from '@nestjs/common';
 import { setSecurityMiddleware } from '@weaver2/common/configs/middleware/security.middleware';
 
 export function setMiddleware<T extends INestApplication>(app: T): void {
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   setSecurityMiddleware(app);
 }
