b7267af1735a176b08fca6fbd83e966d15e98c2f
Centell <centell@me.com>
2025-06-17 09:58:25 +0900

    chore: fix directory name

--- libs/common/src/configs/set-nest-app.config.ts
+++ libs/common/src/configs/set-nest-app.config.ts
@@ -1,6 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import { setMiddleware } from '@weaver2/common/configs/middleware';
-import { setPipe } from '@weaver2/common/configs/Pipe';
+import { setPipe } from 'libs/common/src/configs/pipe';
 
 export function setNestApp<T extends INestApplication>(app: T): void {
   app.enableShutdownHooks();
