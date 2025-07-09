3b6775a07eb308558d0f8151a47e60526b49d983
Centell <centell@me.com>
2025-07-09 17:42:11 +0900

    chore: add color for logs

--- libs/common/src/global/logger/custom.logger.ts
+++ libs/common/src/global/logger/custom.logger.ts
@@ -1,20 +1,21 @@
 import { LoggerService, Injectable } from '@nestjs/common';
+import chalk from 'chalk';
 
 @Injectable()
 export class CustomLogger implements LoggerService {
   log(message: string, context?: string) {
-    console.log(`[LOG${context ? `: ${context}` : ''}] ${message}`);
+    console.log(chalk.green(`[LOG${context ? `: ${context}` : ''}] ${message}`));
   }
   error(message: string, trace: string, context?: string) {
-    console.error(`[ERROR${context ? `: ${context}` : ''}] ${message}`, trace);
+    console.error(chalk.red(`[ERROR${context ? `: ${context}` : ''}] ${message}`), trace);
   }
   warn(message: string, context?: string) {
-    console.warn(`[WARN${context ? `: ${context}` : ''}] ${message}`);
+    console.warn(chalk.yellow(`[WARN${context ? `: ${context}` : ''}] ${message}`));
   }
   debug(message: string, context?: string) {
-    console.debug(`[DEBUG${context ? `: ${context}` : ''}] ${message}`);
+    console.debug(chalk.blue(`[DEBUG${context ? `: ${context}` : ''}] ${message}`));
   }
   verbose(message: string, context?: string) {
-    console.info(`[VERBOSE${context ? `: ${context}` : ''}] ${message}`);
+    console.info(chalk.cyan(`[VERBOSE${context ? `: ${context}` : ''}] ${message}`));
   }
 }
