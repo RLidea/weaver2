1bfa699799af1b96df3e09ae649530b99457937a
Centell <centell@me.com>
2026-02-23 12:33:05 +0900

    refactor(pagination): remove deprecated PaginationService and PaginationModule
    OffsetPaginationService, CursorPaginationService, KeysetPaginationService로
    전환 완료되어 사용처가 없는 deprecated 클래스 및 모듈 삭제

--- libs/pagination/src/index.ts
+++ libs/pagination/src/index.ts
@@ -1,8 +1,3 @@
-// 하위 호환 (deprecated)
-export * from './pagination.module';
-export * from './pagination.service';
-
-// 신규 API
 export * from './common';
 export * from './cursor';
 export * from './keyset';
