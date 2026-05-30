2e306af2f712ece55f448c475fd2574753febd62
Centell <centell@me.com>
2026-05-30 23:54:12 +0900

    feat(module-registry): export public API barrel
    공개 API 배럴 export.

--- libs/module-registry/src/index.ts
+++ libs/module-registry/src/index.ts
@@ -1,1 +1,22 @@
-export {};
+export type {
+  ModuleLayer,
+  DependencyKind,
+  FeatureDependency,
+  FeatureFootprint,
+  RemovalNote,
+  FeatureManifest,
+} from './feature-manifest.type';
+
+export type {
+  GraphNode,
+  GraphEdge,
+  DependencyGraph,
+  RemovalImpact,
+} from './dependency-graph.type';
+
+export {
+  buildDependencyGraph,
+  analyzeRemoval,
+  findCycles,
+  serializeGraph,
+} from './build-graph';
